51e7c1509578a850e1f6b7f175714f9d3087d70b
tbeachill <tbeachill@proton.me>
2024-08-11 17:24:30 +0100

    moved blacklist functions into own file

--- commands/commands.go
+++ commands/commands.go
@@ -1,16 +1,9 @@
 package commands
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/bwmarrin/discordgo"
 
-	"gamestreams/config"
 	"gamestreams/db"
-	"gamestreams/discord"
-	"gamestreams/logs"
-	"gamestreams/utils"
 )
 
 // commandHandlers is a map of command names to their respective handler functions.
@@ -53,55 +46,4 @@ func parseOptions(options []*discordgo.ApplicationCommandInteractionDataOption) 
 		}
 	}
 	return &s
-}
-
-// userIsBlacklisted checks if a user is blacklisted from using the bot.
-// If the user is blacklisted, it sends a DM to the user with the reason for the blacklist.
-func userIsBlacklisted(i *discordgo.InteractionCreate) bool {
-	userID := utils.GetUserID(i)
-	blacklisted, b := db.IsBlacklisted(userID, "user")
-
-	if blacklisted {
-		logs.LogInfo(" CMND", "blacklisted user tried to use command", false,
-			"user", userID,
-			"reason", b.Reason,
-			"command", i.ApplicationCommandData().Name)
-		lastMessaged, timeErr := time.Parse("2006-01-02", b.LastMessaged)
-		if timeErr != nil {
-			logs.LogError(" CMND", "error parsing time",
-				"user", userID,
-				"err", timeErr)
-		}
-		if b.LastMessaged == "" || time.Now().Compare(lastMessaged) > 0 {
-			discord.DM(userID, fmt.Sprintf("You are blacklisted from using this bot.\n\nReason: %s"+
-				"\nExpires: %s ", b.Reason, b.DateExpires))
-			db.UpdateLastMessaged(userID)
-		}
-		return true
-	}
-	return false
-}
-
-// BlacklistIfSpamming checks if a user is spamming commands and blacklists them if they are.
-func BlacklistIfSpamming(i *discordgo.InteractionCreate) {
-	userID := utils.GetUserID(i)
-
-	dCount, err := db.CheckUsageByUser(userID, "-1 day")
-	if err != nil {
-		logs.LogError(" CMND", "error checking command usage",
-			"user", userID,
-			"err", err)
-		return
-	}
-	hCount, err := db.CheckUsageByUser(userID, "-1 hour")
-	if err != nil {
-		logs.LogError(" CMND", "error checking command usage",
-			"user", userID,
-			"err", err)
-		return
-	}
-	if dCount >= config.Values.Blacklist.DailyCommandLimit ||
-		hCount >= config.Values.Blacklist.HourlyCommandLimit {
-		db.AddToBlacklist(userID, "user", "spamming commands", 2)
-	}
 }
